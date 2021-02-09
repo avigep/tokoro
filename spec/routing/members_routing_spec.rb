2a84a6c71d43e201d652d8cd77aea6693179f393
Avi <avi.gep@gmail.com>
2021-02-10 03:24:33 +0530

    view fixes

---
+++ spec/routing/members_routing_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe MembersController, type: :routing do
+  describe "routing" do
+    it "routes to #index" do
+      expect(get: "/members").to route_to("members#index")
+    end
+  end
+end
